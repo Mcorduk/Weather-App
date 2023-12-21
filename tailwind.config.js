cbaac4c58e04a6f2cd81817d733174947ad7dde9
Muhammet Corduk <104722188+Mcorduk@users.noreply.github.com>
2023-12-21 17:06:35 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.js",
+    "./dist/*.html",
+    "./dist/*.js",
+    "./dist/styles/*.css",
+  ], // Update content paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
