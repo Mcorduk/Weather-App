911f997f9f6d8f16834d28263ce1666d78e5892c
Mcorduk <om.corduk@gmail.com>
2023-12-22 13:22:20 +0300

    Seperate DOM modules for user input field and info field.

--- main.js
+++ main.js
@@ -10,23 +10,13 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/scripts/DOM.js":
-/*!****************************!*\
-  !*** ./src/scripts/DOM.js ***!
-  \****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n// DOM stuffs\n\nconst button = document.querySelector(\"button\");\nconst input = document.querySelector(\"input\");\nbutton.addEventListener(\"click\", () => {\n  // fetchCurrentWeather(input.value);\n});\n\n\n//# sourceURL=webpack://webpack-template-repo/./src/scripts/DOM.js?");
-
-/***/ }),
-
 /***/ "./src/scripts/fetch.js":
 /*!******************************!*\
   !*** ./src/scripts/fetch.js ***!
   \******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* Hard coding API Key. \nLearning how to not is not covered in Odin curriculum yet.\nBut I am using Weather API's free plan so it is okay.\n*/\n\nasync function fetchCurrentWeather(location) {\n  // Function to refactor the API responded JSON to usable object\n  function refactorWeatherData(weatherData) {\n    const city = weatherData.location.name;\n    const {\n      country\n    } = weatherData.location;\n    const condition = weatherData.current.condition.text;\n    const tempC = weatherData.current.temp_c;\n    const tempF = weatherData.current.temp_f;\n    const {\n      humidity\n    } = weatherData.current;\n    const feelsLikeC = weatherData.current.feelslike_c;\n    const feelsLikeF = weatherData.current.feelslike_f;\n    const wind = weatherData.current.wind_kph;\n    return {\n      city,\n      country,\n      condition,\n      tempC,\n      tempF,\n      humidity,\n      feelsLikeC,\n      feelsLikeF,\n      wind\n    };\n  }\n  try {\n    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=e6f9f80a7ad94403a0d95411232112&q=${location}`, {\n      mode: \"cors\"\n    });\n    const weatherData = await response.json();\n    const refactoredWeatherData = refactorWeatherData(weatherData);\n    console.log(refactoredWeatherData);\n  } catch (error) {\n    console.log(error);\n  }\n}\nfetchCurrentWeather(\"singapore\");\n\n\n//# sourceURL=webpack://webpack-template-repo/./src/scripts/fetch.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* Hard coding API Key. \nLearning how to not is not covered in Odin curriculum yet.\nBut I am using Weather API's free plan so it is okay.\n*/\n\nasync function fetchCurrentWeather(location) {\n  // Function to refactor the API responded JSON to usable object\n  function refactorWeatherData(weatherData) {\n    const city = weatherData.location.name;\n    const {\n      country\n    } = weatherData.location;\n    const condition = weatherData.current.condition.text;\n    const tempC = weatherData.current.temp_c;\n    const tempF = weatherData.current.temp_f;\n    const {\n      humidity\n    } = weatherData.current;\n    const feelsLikeC = weatherData.current.feelslike_c;\n    const feelsLikeF = weatherData.current.feelslike_f;\n    const wind = weatherData.current.wind_kph;\n    return {\n      city,\n      country,\n      condition,\n      tempC,\n      tempF,\n      humidity,\n      feelsLikeC,\n      feelsLikeF,\n      wind\n    };\n  }\n  try {\n    // Fetch the response for current day's weather data from Weather API\n    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=e6f9f80a7ad94403a0d95411232112&q=${location}`, {\n      mode: \"cors\"\n    });\n    // Turn data into JSON\n    const weatherData = await response.json();\n    // Refactor the data into a more readable object with needed data only.\n    const refactoredWeatherData = refactorWeatherData(weatherData);\n    console.log(refactoredWeatherData);\n  } catch (error) {\n    console.log(error);\n  }\n}\nfetchCurrentWeather(\"singapore\");\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (fetchCurrentWeather);\n\n//# sourceURL=webpack://webpack-template-repo/./src/scripts/fetch.js?");
 
 /***/ }),
 
@@ -36,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* Hard coding API Key. \nLea
   \******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _DOM__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./DOM */ \"./src/scripts/DOM.js\");\n/* harmony import */ var _fetch__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./fetch */ \"./src/scripts/fetch.js\");\n/* harmony import */ var _renderImg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./renderImg */ \"./src/scripts/renderImg.js\");\n/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./theme */ \"./src/scripts/theme.js\");\n\n\n\n\n\n//# sourceURL=webpack://webpack-template-repo/./src/scripts/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './DOM'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n/* harmony import */ var _fetch__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./fetch */ \"./src/scripts/fetch.js\");\n/* harmony import */ var _renderImg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./renderImg */ \"./src/scripts/renderImg.js\");\n/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./theme */ \"./src/scripts/theme.js\");\n\n\n\n\n\n//# sourceURL=webpack://webpack-template-repo/./src/scripts/index.js?");
 
 /***/ }),
 
@@ -87,6 +77,23 @@ eval("__webpack_require__.r(__webpack_exports__);\n// Dark - Light Theme Toggle 
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
