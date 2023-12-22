3975752b02b4e6df4396ef9694cf50d2bf0ec279
Mcorduk <om.corduk@gmail.com>
2023-12-22 10:59:34 +0300

    Ran prettier on the latest html and script

--- src/fetch.js
+++ src/fetch.js
@@ -1,18 +1,18 @@
-
 // Hard coding API Key. Learning how to not do this is not covered yet, using free plan.
 const city = document.getElementById("city").innerHTML;
 const date = document.getElementById("city").innerHTML;
 
-
-
 async function fetchCurrentWeather(location) {
-  const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=e6f9f80a7ad94403a0d95411232112&q=${location}`, {mode: "cors"});
+  const promise = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=e6f9f80a7ad94403a0d95411232112&q=${location}`,
+    { mode: "cors" },
+  );
   const dataJSon = await promise.json();
   console.log(dataJSon);
 
   const city = dataJSon.location.name;
 
-  const {country} = dataJSon.location;
+  const { country } = dataJSon.location;
 
   const condition = dataJSon.current.condition.text;
 
@@ -20,14 +20,13 @@ async function fetchCurrentWeather(location) {
 
   const tempF = dataJSon.current.temp_f;
 
-  const {humidity} = dataJSon.current;
+  const { humidity } = dataJSon.current;
 
   const feelsLikeC = dataJSon.current.feelslike_c;
 
   const feelsLikeF = dataJSon.current.feelslike_f;
 
   const wind = dataJSon.current.wind_kph;
-  
 }
 
 // `https://api.weatherapi.com/v1/current.json?key=e6f9f80a7ad94403a0d95411232112&q=${city}`
@@ -36,7 +35,8 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 
 button.addEventListener("click", () => {
-  fetchCurrentWeather(input.value);});
+  fetchCurrentWeather(input.value);
+});
 
 export { };
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import { } from "./fetch";
-
+import {} from "./fetch";
