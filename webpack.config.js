17fc186bd46a4ec2c3592fd992b131a01ca4b5ce
Mcorduk <om.corduk@gmail.com>
2023-12-22 20:28:46 +0300

    Edit webpack to productipn

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/scripts/index.js",
-  mode: "development",
+  mode: "production",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
